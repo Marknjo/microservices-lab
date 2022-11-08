541c6dc22a6e7280a1fc43bd5759f11813ea8f07
Marknjo <marknjo7@gmail.com>
2022-11-08 14:06:06 +0300

    feat(ms-lab): initial implementation of user model - auth api

---
+++ auth/src/models/user.ts
@@ -0,0 +1,45 @@
+import mongoose, { Document, Model } from 'mongoose';
+const { Schema, model } = mongoose;
+
+/// Properties required to create a new user
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+/// An interface that describes the properties that a User Model has
+interface UserModel extends Model<any> {
+  build(attrs: UserAttrs): any;
+}
+
+/// An interface that describe the properties that a User Document has
+interface UserDoc extends Document {
+  email: string;
+  password: string;
+  updatedAt: string;
+  createdAt: string;
+}
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'User must have email'],
+    },
+    password: {
+      type: String,
+      required: [true, 'User must have a password'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
